d20991aa90f3693e1592d4ec32dc89ad3a0ef182
Mohd Danish Sheikh <danish@lgads.tv>
2023-02-27 23:02:13 +0530

    init

--- example.go
+++ example.go
@@ -2,10 +2,13 @@ package example
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"path"
 	"path/filepath"
 	"runtime"
+
+	"github.com/integralist/go-findroot/find"
 )
 
 func DirPath() string {
@@ -18,5 +21,14 @@ func WDPath() (string, error) {
 	fmt.Println(filepath.Abs("."))
 	fmt.Println(path.Dir("."))
 	fmt.Println(os.Getwd())
+	fmt.Println(os.Executable())
+
+	root, err := find.Repo()
+	if err != nil {
+		log.Fatalf("Error: %s", err.Error())
+	}
+
+	fmt.Printf("%+v", root)
+
 	return filepath.Abs(".")
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/dan78692/test-go
 
 go 1.19
+
+require github.com/integralist/go-findroot v0.0.0-20160518114804-ac90681525dc
